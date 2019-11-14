2ffb61d149a41ecf6033e3dac239938d37dfa92d
thikade <thomas.hikade@gmail.com>
2019-11-14 08:36:48 +0100

    optimized jmeter docker build; upgrade to v5.2; add HEAP env var

--- jmeter/entrypoint.sh
+++ jmeter/entrypoint.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+env| grep JMETER
+
+echo "-------------------------------"
+echo "new JMETER HEAP=$HEAP"
+echo "-------------------------------"
+
 export PATH=$PATH:${JMETER_BIN}
-#jmeter -n -t test/attempt1.jmx
+echo PATH=$PATH
 jmeter -n -t test/${JMETER_TEST}
---
+++ jmeter/setenv.sh
@@ -0,0 +1,4 @@
+set JAVA_HOME=C:\Apps\JDK8-64
+set PATH=%JAVA_HOME%\bin;%PATH%
+echo "setEnv.bat called"
+set HEAP=-Xms3g -Xmx3g
