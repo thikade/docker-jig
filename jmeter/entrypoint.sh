16a81817fb2a0e3ea64ce8fcab44f4d38eef81d0
thikade <thomas.hikade@gmail.com>
2019-11-20 21:46:38 +0100

    testplan & user.props moved to docker volume /data

--- jmeter/entrypoint.sh
+++ jmeter/entrypoint.sh
@@ -2,14 +2,19 @@
 
 export PATH=$PATH:${JMETER_BIN}
 
-env| grep JMETER
+env | grep JMETER
 
 echo "-------------------------------"
 echo "new JMETER HEAP=$HEAP"
 echo "-------------------------------"
-echo "running jmx file:  test/${JMETER_TEST}"
-echo "-------------------------------"
 
-jmeter -n -t test/${JMETER_TEST}  -l results_${JMETER_TEST}_$(date +%Y%m%d-%H.%M.%S).jtl
+if [ -f "/data/${JMETER_PLAN}" ]; then
+  echo "running jmx file:  /data/${JMETER_PLAN}"
+  echo "-------------------------------"
 
-tail -f /jmeter/jmeter.log
+  jmeter -n -t /data/${JMETER_PLAN}  -l /data/results_${JMETER_PLAN}_$(date +%Y%m%d-%H.%M.%S).jtl
+else
+  echo "no test plan found to run! (tried: \"/data/${JMETER_PLAN}\""
+fi
+
+[ "$1" = "tail" ] && tail -F /jmeter/jmeter.log
