90bfb4beacde2660067505fd33f25c2f8343a8eb
thikade <thomas.hikade@gmail.com>
2019-11-26 11:46:27 +0100

    better log file location and fix global user.properties issue

--- jmeter/run.sh
+++ jmeter/run.sh
@@ -2,7 +2,7 @@
 
 export PATH=$PATH:${JMETER_BIN}
 
-env | grep JMETER
+env | sort | grep JMETER
 
 echo "-------------------------------"
 echo "new JMETER HEAP=$HEAP"
@@ -13,19 +13,21 @@ echo "startup script name is: $0"
 echo "args are              : $*"
 echo "-------------------------------"
 
-# script is called using "entrypoint.sh" ...
+TS_FORMAT=+%Y-%m-%d-%H.%M.%S
+TS=$(date $TS_FORMAT)
+PREFIX=${JMETER_PLAN}_${TS}
+JM_LOG=$(dirname $JMETER_PLAN)/jmeter.log
+REPORT_DIR=${PREFIX}_report
+SAMPLES_LOG=${PREFIX}_samples.jtl
+
+# if script is called using "entrypoint.sh", just tail the JMeter logs!
 if [ "$(basename $0)" = "entrypoint.sh" ]
 then
   # container startup will tail log forever
-  echo "tailing jmeter log ..."
+  echo "$TS: tailing jmeter log(s): \"$JM_LOG\" ..."
   echo "-------------------------------"
-  while true; do
-    tail -F /jmeter/jmeter.log 2>/dev/null
-    echo "tailing jmeter log ..."
-    sleep 5
-  done
+  tail -F $JM_LOG  2>/dev/null
 fi
-
 
 # script is called using "run" ...
 
@@ -34,17 +36,25 @@ then
   # this is a slave container
   echo "starting the JMeter SLAVE server process ..."
   jmeter-server &
-  tail -F /jmeter/jmeter-server.log
+  sleep 3
+  # slaves use their own log file
+  tail -F /jmeter/jmeter-server.log 2>/dev/null
 
 elif [ "$JMETER_ROLE" = "MASTER" ]
 then
-  TS=$(date +%Y%m%d-%H.%M.%S)
   if [ -f "$JMETER_PLAN" ]
   then
+    # test -d $REPORT_DIR || mkdir $REPORT_DIR
+    echo -e "\n\n"
+    echo "=> user.props: number of threads:  $(grep threads=  /data/user.properties)"
+    echo "=> user.props: duration         :  $(grep duration= /data/user.properties)"
+    echo "=> user.props: rampUp           :  $(grep rampUp=   /data/user.properties)"
+    echo -e "\n\n"
     echo "$TS : starting the JMeter MASTER process ..."
-    test -d /data/$TS || mkdir /data/$TS
-    jmeter -n -t ${JMETER_PLAN}  -l ${JMETER_PLAN}.results-$(date +%Y%m%d-%H.%M.%S).jtl -e -o /data/$TS  ${JMETER_REMOTE_SERVERS} $*
-    echo "$TS : finished."
+    test -n "$DEBUG" && set -x
+    jmeter -n -t ${JMETER_PLAN} -j ${JM_LOG} -l ${SAMPLES_LOG} -e -o $REPORT_DIR -G/data/user.properties ${JMETER_REMOTE_SERVERS} $*
+    set +x
+    echo "$(date $TS_FORMAT) : finished JMeter run."
   else
     echo "ERROR: unable to read JMeter plan: \"$JMETER_PLAN\" !"
   fi
